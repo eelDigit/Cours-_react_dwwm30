2be98f266ddba882448b66aa818f5e687308d101
eel <eeldigit@gmail.com>
2023-04-11 16:22:07 +0200

    add exo1

---
+++ projet_cours/src/Personne.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+
+// function Personne(props){
+//     return(
+//         <>
+//             <h1> {props.nom} </h1>
+//             <p>Age : {props.age} </p>
+//             <p>Sex : {props.sex} </p>
+//         </>
+//     )
+// }
+
+// ---------------------------------------
+//  const Personne = (props) =>{
+//     return(
+//         <>
+//             <h1> {props.nom} </h1>
+//             <p>Age : {props.age} </p>
+//             <p>Sex : {props.sex} </p>
+//         </>
+//     )
+// }
+
+
+class Personne extends Component {
+
+    // constructor(props){
+    //     super(props);
+    // }
+
+
+    render() {
+        return (
+            <>
+                <h1> {this.props.nom} </h1>
+                <p>Age : {this.props.age} </p>
+                <p>Sex : {this.props.sex} </p>
+            </>
+
+        );
+    }
+}
+
+
+
+
+
+export default Personne;
+
+
